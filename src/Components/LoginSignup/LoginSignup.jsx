19fbb7f032382b6e755dc9e67f5d146d1cf46d58
MAX M <tamunowari93@gmail.com>
2025-08-10 14:39:18 -0700

    Add SignupLogin Component setup 02

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,12 +1,30 @@
-ReadableStreamDefaultController
 import React from 'react';
-import './LoginSignup.css'; // Assuming you have a CSS file for styling
+import './LoginSignup.css';
+import user_icon from '../Assets/user.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
 const LoginSignup = () => {
   return (
-    <div>
-      <h1>Login or Signup</h1>
-      {/* Add your login/signup form here */}
-
+    <div className='container'>
+      <div className="header">
+        <div className="text">SignUp</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" />
+        </div>
+        <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" />
+        </div>
+        <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" />
+        </div>
+      </div>
     </div>
   );
 }

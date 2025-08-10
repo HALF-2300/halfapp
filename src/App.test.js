5e57617e88dcda41b6a4a4edcf430cfa74dcc885
MAX M <tamunowari93@gmail.com>
2025-08-10 14:46:53 -0700

    Add SignupLogin Component setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders half app loading text', () => {
+
+test('renders LoginSignup component', () => {
   render(<App />);
-  const loadingText = screen.getByText(/Half App Loading.../i);
-  expect(loadingText).toBeInTheDocument();
+  const signupText = screen.getByText(/SignUp/i);
+  expect(signupText).toBeInTheDocument();
 });
+
+test('renders input fields', () => {
+  render(<App />);
+  const userInput = screen.getByPlaceholderText(/Username/i);
+  const emailInput = screen.getByPlaceholderText(/Email/i);
+  const passwordInput = screen.getByPlaceholderText(/Password/i);
+  
+  expect(userInput).toBeInTheDocument();
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
+});
+
+export default App;

0430c0778be937ffdfc309ce7db70f0b3d1b90f4
MAX M <tamunowari93@gmail.com>
2025-08-11 02:03:03 -0700

    Add SignupLogin Frontend

--- src/App.test.js
+++ src/App.test.js
@@ -2,21 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 
-// test('renders LoginSignup component', () => {
-//   render(<App />);
-//   const signupText = screen.getByText(/SignUp/i);
-//   expect(signupText).toBeInTheDocument();
-// });
-
-// test('renders input fields', () => {
-//   render(<App />);
-//   const userInput = screen.getByPlaceholderText(/Username/i);
-//   const emailInput = screen.getByPlaceholderText(/Email/i);
-//   const passwordInput = screen.getByPlaceholderText(/Password/i);
-  
-//   expect(userInput).toBeInTheDocument();
-//   expect(emailInput).toBeInTheDocument();
-//   expect(passwordInput).toBeInTheDocument();
-// });
+test('renders login/signup component', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Login/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
 export default App;
